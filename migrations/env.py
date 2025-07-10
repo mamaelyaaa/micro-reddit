df2be564c765ca75232460ff1ec6ebf3e3eb937d
mamaelyaaa <starikovskyye4@gmail.com>
2025-07-10 23:03:08 +0500

    create post model with alembic revision

--- migrations/env.py
+++ migrations/env.py
@@ -12,6 +12,7 @@ from models import Base
 
 # Models
 from api.auth.users.models import User
+from api.posts.models import Post
 
 config = context.config
 
--- src/api/posts/models.py
+++ src/api/posts/models.py
@@ -1,6 +1,23 @@
+from typing import Optional
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql.schema import UniqueConstraint, ForeignKey
+from sqlalchemy.types import String
+
 from models import Base, DateMixin
 
 
 class Post(Base, DateMixin):
     __tablename__ = "posts"
 
+    user_id: Mapped[str] = mapped_column(ForeignKey("users.id"))
+    title: Mapped[str] = mapped_column(String(128))
+    description: Mapped[Optional[str]] = mapped_column(String(2048))
+
+    __table_args__ = (
+        UniqueConstraint(
+            "title",
+            "user_id",
+            name="unique_title_with_user",
+        ),
+    )

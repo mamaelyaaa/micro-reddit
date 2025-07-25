9ff0425a989fabf560046a71776829d819c88e6e
mamaelyaaa <starikovskyye4@gmail.com>
2025-07-25 16:56:46 +0500

    add taskiqDep

--- src/api/tasks/feed_tasks.py
+++ src/api/tasks/feed_tasks.py
@@ -1,15 +1,21 @@
 import logging
+from typing import Annotated
+
+from taskiq import TaskiqDepends
 
 from api.feeds.models import FeedType
-from api.feeds.service import FeedServiceDep, FeedServiceProtocol
+from api.feeds.service import FeedServiceProtocol, get_feed_service
 from core.broker import broker
 
-logger = logging.getLogger("tasks")
+logger = logging.getLogger("feed_tasks")
+
+
+FeedServiceTaskiqDep = Annotated[FeedServiceProtocol, TaskiqDepends(get_feed_service)]
 
 
 @broker.task(task_name="create_event_for_users")
 async def create_event_for_users(
-    feed_service: FeedServiceProtocol,
+    feed_service: FeedServiceTaskiqDep,
     author_id: int,
     event_id: int,
     event_type: FeedType,
@@ -18,4 +24,5 @@ async def create_event_for_users(
         f"Отправляем задачу на обновление событий {author_id = }, {event_id = }, event_type = {event_type.upper()}"
     )
     await feed_service.create_event_for_users(author_id, event_id, event_type)
+    logger.info(f"Задача create_event_for_users выполнена")
     return
--- src/core/logger.py
+++ src/core/logger.py
@@ -29,37 +29,4 @@ def setup_logging() -> None:
     logging.getLogger("asyncio").setLevel(logging.WARNING)
     logging.getLogger("aio_pika").setLevel(logging.INFO)
     logging.getLogger("aiormq").setLevel(logging.INFO)
-
-
-class MicroRedditLogger:
-
-    def __init__(self, log_level: Literal["DEBUG", "INFO"], log_format: str):
-        self.log_level = log_level
-        self.log_format = log_format
-        self._logger_startup()
-
-    @staticmethod
-    def _logger_startup():
-        settings.files.logs_dir.mkdir(exist_ok=True)
-
-        root_logger = logging.getLogger()
-        root_logger.setLevel(settings.log.level)
-
-        formatter = logging.Formatter(settings.log.format)
-
-        stream_handler = logging.StreamHandler(sys.stdout)
-        stream_handler.setFormatter(formatter)
-
-        file_handler = logging.FileHandler(settings.files.logs_file, encoding="utf-8")
-        file_handler.setFormatter(formatter)
-
-        if root_logger.hasHandlers():
-            root_logger.handlers.clear()
-
-        root_logger.addHandler(stream_handler)
-        root_logger.addHandler(file_handler)
-
-
-app_logger = MicroRedditLogger(
-    log_level=settings.log.level, log_format=settings.log.format
-)
+    logging.getLogger("taskiq.receiver").setLevel(logging.INFO)
